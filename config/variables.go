d3b829cd936d6f1f2d5e8024bf75064791c273cb
linrunxin <linrunxin@xiaohongshu.com>
2026-04-25 19:51:19 +0800

    refactor(config): 扩展 Config 字段并改造懒加载 --revanxAiCoding

--- config/variables.go
+++ config/variables.go
@@ -7,20 +7,30 @@ import (
 )
 
 type Config struct {
-	DBDriver   string `mapstructure:"DB_DRIVER"`
-	DBSource   string `mapstructure:"DB_SOURCE"`
-	ServerPort string `mapstructure:"SERVER_PORT"`
-	JwtPriKey  string `mapstructure:"JWT_PRIVATE_KEY"`
+	ServerPort            string `mapstructure:"SERVER_PORT"`
+	JwtPriKey             string `mapstructure:"JWT_PRIVATE_KEY"`
+	JwtDurationHours      int    `mapstructure:"JWT_DURATION_HOURS"`
+	DBDriver              string `mapstructure:"DB_DRIVER"`
+	DBSource              string `mapstructure:"DB_SOURCE"`
+	OSSRegion             string `mapstructure:"OSS_REGION"`
+	OSSEndpoint           string `mapstructure:"OSS_ENDPOINT"`
+	OSSBucketFiles        string `mapstructure:"OSS_BUCKET_FILES"`
+	OSSBucketAvatars      string `mapstructure:"OSS_BUCKET_AVATARS"`
+	OSSAccessKeyID        string `mapstructure:"OSS_ACCESS_KEY_ID"`
+	OSSAccessKeySecret    string `mapstructure:"OSS_ACCESS_KEY_SECRET"`
+	OSSStsRoleArn         string `mapstructure:"OSS_STS_ROLE_ARN"`
+	OSSStsDurationSeconds int    `mapstructure:"OSS_STS_DURATION_SECONDS"`
+	ModelServerBaseURL    string `mapstructure:"MODEL_SERVER_BASE_URL"`
+	ModelServerTimeoutSec int    `mapstructure:"MODEL_SERVER_TIMEOUT_SECONDS"`
+	TaskWorkerPoolSize    int    `mapstructure:"TASK_WORKER_POOL_SIZE"`
+	TaskQueueCapacity     int    `mapstructure:"TASK_QUEUE_CAPACITY"`
 }
 
 var (
 	config Config
 	once   sync.Once
+	loaded bool
 )
-
-func init() {
-	LoadConfig(".")
-}
 
 func LoadConfig(path string) {
 	once.Do(func() {
@@ -36,12 +46,13 @@ func LoadConfig(path string) {
 		if err != nil {
 			panic(err)
 		}
+		loaded = true
 	})
 }
 
 func GetConfig() Config {
-	if config == (Config{}) {
-		panic("config is not loaded")
+	if !loaded {
+		LoadConfig(".")
 	}
 	return config
 }
--- config/variables_test.go
+++ config/variables_test.go
@@ -6,14 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
-
 func TestGetConfig(t *testing.T) {
-
 	config := GetConfig()
-	// assert.Equal(t, config.DBDriver, "postgres")
-	// assert.Equal(t, config.DBSource, "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable")
-	// assert.Equal(t, config.ServerPort, "8080")
-	assert.NotNil(t, config.JwtPriKey)
+	assert.NotEmpty(t, config.JwtPriKey)
 	assert.Equal(t, "bcuwehjfihafjkbjnacc", config.JwtPriKey)
+	assert.Equal(t, ":8888", config.ServerPort)
+	assert.Equal(t, 24, config.JwtDurationHours)
 }
