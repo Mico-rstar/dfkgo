be2c260932a2fda389aa9833fdb123e4b3474a20
linrunxin <linrunxin@xiaohongshu.com>
2026-04-26 10:43:29 +0800

    refactor(main): 改用 http.Server 实现 HTTP 优雅关闭 --revanxAiCoding

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"dfkgo/api"
 	"dfkgo/config"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -20,12 +23,28 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
+	srv := &http.Server{
+		Addr:    cfg.ServerPort,
+		Handler: server.Router(),
+	}
+
 	go func() {
-		<-quit
-		log.Println("[main] shutting down workers...")
-		server.StopWorkers()
-		os.Exit(0)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
 	}()
 
-	server.Start(cfg.ServerPort)
+	log.Printf("[main] server started on %s\n", cfg.ServerPort)
+
+	<-quit
+	log.Println("[main] shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("[main] HTTP server shutdown error: %v", err)
+	}
+
+	server.StopWorkers()
+	log.Println("[main] server exited")
 }
